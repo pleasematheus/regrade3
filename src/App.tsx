89da3fb08962a17b365b2c8976a6a0cc43db1097
pleasematheus <matheus4k@gmail.com>
2024-10-11 22:25:22 -0400

    Update color scheme, add meta tags, and version bump to version 0.10.0

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
     <div className="grid place-items-center h-svh">
       <h1 className="cg-black text-9xl">Regra de 3</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,25 @@ export default {
   plugins: [
     require('daisyui'),
   ],
-}
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#2EC4B6",
+          secondary: "#E71D36",
+          accent: "#FF9F1C",
+          neutral: "#7F8B92",
+          "base-100": "#FDFFFC"
 
+        },
+        dark: {
+          primary: "#2EC4B6",
+          secondary: "#E71D36",
+          accent: "#FF9F1C",
+          neutral: "#7F8B92",
+          "base-100": "#011627"
+        }
+      }
+    ],
+  },
+}
