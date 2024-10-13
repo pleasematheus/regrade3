d3e049c5747b513bc3ddb270c95e1a86ad141ae2
pleasematheus <matheus4k@gmail.com>
2024-10-13 16:36:32 -0400

    Bump version to 1.3.0 and add Footer component with styling for improved layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full bg-gray-800 text-white text-center py-2">
+      <p>© 2024. Todos os direitos reservados.</p>
+      <p>
+        Feito com ❤️ e carinho por{" "}
+        <a
+          href="http://https://github.com/pleasematheus"
+          target="_blank"
+          className="text-blue-500"
+        >
+          pleasematheus
+        </a>
+      </p>
+      <p>;)</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import Inputs from "@/components/Inputs"
+import Footer from "@/components/Footer"
 
 function App() {
   return (
     <>
       <Header />
       <Inputs />
+      <Footer/>
     </>
   )
 }
