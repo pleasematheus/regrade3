d57c18bf15e3302e13492426fd87735b3dc951cb
pleasematheus <matheus4k@gmail.com>
2024-10-13 16:36:47 -0400

    Bump version to 1.3.1, update Footer component styling, and add new background utility classes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-full bg-gray-800 text-white text-center py-2">
+    <footer className="fixed bottom-0 w-full bg-base-200 text-center py-2">
       <p>© 2024. Todos os direitos reservados.</p>
       <p>
         Feito com ❤️ e carinho por{" "}
